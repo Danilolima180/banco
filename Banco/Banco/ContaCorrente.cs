e4b48e6225d75762db43a7a91818dfc167918d00
Danilo Rodrigues De Lima <38114256+Danilolima180@users.noreply.github.com>
2023-02-02 11:54:53 -0300

    final da aula 02/02/2023

---
+++ Banco/Banco/ContaCorrente.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Banco;
+public class ContaCorrente
+{
+    public string nome;
+    public int agencia;
+    public string conta;
+    public double saldo, transferir;
+    public double valortranferencia;
+    public bool sacar(double valor)
+    {
+        if (this.saldo >= valor)
+        {
+            this.saldo -= valor;
+            return true;
+        }
+        else
+        {
+            Console.WriteLine("Saldo insuficiente para o saque.");
+            return false;
+        }
+    }
+    public bool Transferir(double valor, ContaCorrente contaDestino)
+    {
+        valortranferencia = valor;
+        if (this.saldo >= valor)
+        {
+            this.saldo -= valor;
+            contaDestino.saldo += valor;
+            return true;
+        }
+        else
+        {
+            Console.WriteLine("Não foi possivel realizar a transferencia. O saldo é insficiente");
+            return false;
+        }
+    }
+}
---
+++ Banco/Banco/Program.cs
@@ -0,0 +1,32 @@
+﻿using Banco;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Banco
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta2 = new ContaCorrente();
+            conta2.agencia = 1812;
+            conta2.conta = "51648-5";
+            conta2.nome = "Danilo"; conta2.saldo = 4000;
+            // Console.WriteLine("O saldo anterior é: R$" + conta2.saldo);
+            //if (conta2.sacar(1000))
+            // Console.WriteLine("O saldo depois do saque é: " + conta2.saldo); 
+           ContaCorrente conta1 = new ContaCorrente();
+            conta1.agencia = 5500;
+            conta1.conta = "203156-a";
+            conta1.nome = "Lucas";
+            conta1.saldo = 250;
+            Console.WriteLine("SALDO ATUAL DA CONTA QUE IRÁ TRANSFERIR: " + conta2.saldo);
+            Console.WriteLine("SALDO ATUAL DA CONTA QUE IRÁ RECEBER: " + conta1.saldo); if (conta2.Transferir(20, conta1))
+                Console.WriteLine("SALDO ATUAL APÓS VOCÊ TRANSFERIR: " + conta2.saldo);
+            Console.WriteLine("SEU SALDO APOS RECEBER A TRANSFERENCIA " + conta1.saldo);
+            Console.WriteLine("O VALOR DA TRANSFERENCIA FOI DE: " + conta2.valortranferencia);
+        }
+    }
+}
